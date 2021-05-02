196f1a1934f0a302ffb6e2e19b13694537cdce4d
petifox <73014276+petifox@users.noreply.github.com>
2021-05-02 10:22:14 +0200

    NaughtyAttributes, input system, character controller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using NaughtyAttributes;
 
 public class PlayerController : MonoBehaviour
 {
+    [ReadOnly]
+    public Vector3 rawInputMovement;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(rawInputMovement);
+    }
+    public void onMovement(InputAction.CallbackContext value)
+    {
+        Vector2 inputMovement = value.ReadValue<Vector2>();
+        rawInputMovement = new Vector3(inputMovement.x, inputMovement.y, 0);
     }
 }
