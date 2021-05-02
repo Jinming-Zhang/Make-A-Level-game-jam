284cc45374d59b6ab383857962eb959ba2e51dab
petifox <73014276+petifox@users.noreply.github.com>
2021-05-02 23:23:15 +0200

    demo realy done

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,7 +9,12 @@ public class PlayerController : MonoBehaviour
 {
     [ReadOnly]
     public Vector3 rawInputMovement;
+    public float speed;
     private Rigidbody rb;
+    public GameObject left;
+    public GameObject rigt;
+    public GameObject up;
+    public GameObject down;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +24,39 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(-rawInputMovement);
+        rb.AddForce(rawInputMovement * speed * Time.deltaTime);
+        if(rawInputMovement.x > 0)
+        {
+            left.SetActive(true);
+            rigt.SetActive(false);
+        }
+        else if (rawInputMovement.x != 0)
+        {
+            left.SetActive(false);
+            rigt.SetActive(true);
+        }
+        else
+        {
+            left.SetActive(false);
+            rigt.SetActive(false);
+        }
+
+
+        if (rawInputMovement.y > 0)
+        {
+            down.SetActive(true);
+            up.SetActive(false);
+        }
+        else if(rawInputMovement.y != 0)
+        {
+            down.SetActive(false);
+            up.SetActive(true);
+        }
+        else
+        {
+            down.SetActive(false);
+            up.SetActive(false);
+        }
     }
     public void onMovement(InputAction.CallbackContext value)
     {
