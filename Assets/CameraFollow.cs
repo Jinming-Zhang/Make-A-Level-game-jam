8119320495acf7943658d0abd792ee67f39389b0
petifox <73014276+petifox@users.noreply.github.com>
2021-05-02 11:55:51 +0200

    follow script updates

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -6,16 +6,15 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     
     [Range(0,10)]
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 1;
     public Vector3 offset;
 
     private void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Vector3.Distance(transform.position, desiredPosition) / 100);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, Vector3.Distance(transform.position, desiredPosition) / smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
 
         transform.LookAt(target);
-        print(Vector3.Distance(transform.position, desiredPosition));
     }
 }
