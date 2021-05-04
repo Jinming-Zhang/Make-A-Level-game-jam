55b5616df791116dfb9ae1bd75a6442a9bc1d37b
Drillu <maxmax98765@yahoo.ro>
2021-05-04 22:41:04 +0300

    Background toggle

--- Assets/Scripts/BGDropdownScript.cs
+++ Assets/Scripts/BGDropdownScript.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class BGDropdownScript : MonoBehaviour
 {
     private int currentBG;
     public Button leftArrow;
     public Button rightArrow;
+    
+    public TextMeshProUGUI textToChange;
+ 
 
     private void Awake()
     {
         SelectBackground(0);
+        
     }
 
     private void SelectBackground(int _index)
@@ -21,13 +26,28 @@ public class BGDropdownScript : MonoBehaviour
 
         for(int i = 0;i<transform.childCount;i++)
         {
-            transform.GetChild(i).gameObject.SetActive(i == _index);
+            //transform.GetChild(i).gameObject.SetActive(i == _index);
+
+
+            if (i == _index)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+                textToChange.SetText(transform.GetChild(i).gameObject.name); 
+                
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
         }
+        
+
     }
 
     public void ChangeBackground(int _change)
     {
         currentBG += _change;
         SelectBackground(currentBG);
+        
     }
 }
